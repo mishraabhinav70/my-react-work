f9bb2c43aecde42f608dfa39185b361af1d32a33
abhinav <mishraabhinav7054@gmail.com>
2025-02-28 22:58:25 +0530

    changes in app.js for creating counter

--- usestate/src/App.js
+++ usestate/src/App.js
@@ -1,23 +1,14 @@
-import logo from './logo.svg';
 import './App.css';
-
+import { useState,React } from 'react';
 function App() {
+  const [count,setcount]=useState(0);
+  function counter() {
+    setcount(count+1)
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>counter is {count} </h1>
+      <button onClick={counter}>click</button>
     </div>
   );
 }
