34d5f121fde04be5727749c4de9d0a7f3b4e658e
abhinav <mishraabhinav7054@gmail.com>
2025-03-02 15:10:38 +0530

    practice for useEffect

--- usestate/src/App.js
+++ usestate/src/App.js
@@ -1,14 +1,33 @@
 import './App.css';
-import { useState,React } from 'react';
+import { useState,useEffect,React } from 'react';
 function App() {
+
   const [count,setcount]=useState(0);
+  const [data,setData]=useState("Ram");
+
+  useEffect(
+    ()=>{
+      console.log("updated")
+    },[data]
+  )
+
   function counter() {
     setcount(count+1)
   }
+
+  function updateData() {
+    setData("Sita")
+  }
+
   return (
     <div className="App">
+
       <h1>counter is {count} </h1>
       <button onClick={counter}>click</button>
+
+      <h1>the name is {data} </h1>
+      <button onClick={updateData}>click to update</button>
+
     </div>
   );
 }
