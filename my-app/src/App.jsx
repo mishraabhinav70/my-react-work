6435009fe9b77481dc8f4c68aad130b8422fd154
abhinav <mishraabhinav7054@gmail.com>
2025-03-12 22:28:36 +0530

    add a new project

---
+++ my-app/src/App.jsx
@@ -0,0 +1,13 @@
+import './App.css'
+import About from './assets/About'
+import Hello from './assets/Hello'
+import Page from './assets/Page'
+function App() {
+  return (
+    <>
+<Page/>
+     </>
+  )
+}
+
+export default App
---
+++ my-app/src/assets/Page.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import Hello from './Hello'
+import About from './About'
+
+const Page = () => {
+  return (
+    <div>
+      <Hello/>
+      <About/>
+    </div>
+  )
+}
+
+export default Page
